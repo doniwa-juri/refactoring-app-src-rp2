3e12d71acd1bfde321c35f1c9dae4cfd75a3b1b2
j-doniwa <juri-doniwa@kiseki.systems>
2025-09-05 15:34:54 +0900

    fix: MainSystem.java メッセージを定数化
    メニューを表示するメッセージを定数化し、ConstantMsgクラスへ移行

--- refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
+++ refactoring_app_name/src/jp/co/sss/crud/main/MainSystem.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 
 import jp.co.sss.crud.db.DBController;
+import jp.co.sss.crud.util.ConstantMsg;
 
 /**
  * 社員情報管理システム開始クラス 社員情報管理システムはこのクラスから始まる。<br/>
@@ -34,16 +35,7 @@ public class MainSystem {
 
 		do {
 			// メニューの表示
-			System.out.println("=== 社員管理システム ===");
-			System.out.println("1.全件表示");
-			System.out.println("2.社員名検索");
-			System.out.println("3.部署ID検索");
-			System.out.println("4.新規登録");
-			System.out.println("5.更新");
-			System.out.println("6.削除");
-			System.out.println("7.終了");
-			System.out.print("メニュー番号を入力してください：");
-
+			System.out.print(ConstantMsg.MSG_MANU);
 			// メニュー番号の入力
 			String menuNoStr = br.readLine();
 			menuNo = Integer.parseInt(menuNoStr);
--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -2,4 +2,16 @@ package jp.co.sss.crud.util;
 
 public class ConstantMsg {
 
+	// メニューの表示
+	public static final String MSG_MANU = """
+			=== 社員管理システム ===
+			1.全件表示
+			2.社員名検索
+			3.部署ID検索
+			4.新規登録
+			5.更新
+			6.削除
+			7.終了
+			メニュー番号を入力してください：""";
+
 }
