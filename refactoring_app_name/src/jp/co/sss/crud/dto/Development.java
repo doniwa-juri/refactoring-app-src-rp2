fab2f8d81d9a81d26df34ec931982d52ecc469c0
j-doniwa <juri-doniwa@kiseki.systems>
2025-09-09 13:31:59 +0900

    add: Development.java, Employee.java DTOクラスを作成
    狙い：保守性、拡張性の向上

--- refactoring_app_name/src/jp/co/sss/crud/dto/Development.java
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Development.java
@@ -1,5 +1,28 @@
 package jp.co.sss.crud.dto;
 
 public class Development {
+	private int deptId;
+	private String deptName;
+
+	public Development(int deptId, String deptName) {
+		this.deptId = deptId;
+		this.deptName = deptName;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
 
 }
--- refactoring_app_name/src/jp/co/sss/crud/dto/Employee.java
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Employee.java
@@ -1,5 +1,58 @@
 package jp.co.sss.crud.dto;
 
 public class Employee {
+	private int empId;
+	private String empName;
+	private int gender;
+	private String birthday;
+	private int deptId;
+
+	public Employee(int empId, String empName, int gender, String birthday, int deptId) {
+		this.empId = empId;
+		this.empName = empName;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.deptId = deptId;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
 
 }
