5bc0ed99d8f60d8edece5fd004c1fc94e80a52d0
j-doniwa <juri-doniwa@kiseki.systems>
2025-09-10 11:53:27 +0900

    add: EmployeeFindByEmpNameService.java 作成
    狙い：拡張性の向上
    社員名検索のサービスクラスを作成

---
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeFindByEmpNameService.java
@@ -0,0 +1,26 @@
+package jp.co.sss.crud.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
+import jp.co.sss.crud.io.ConsoleWriter;
+import jp.co.sss.crud.io.EmployeeNameReader;
+
+public class EmployeeFindByEmpNameService implements IEmployeeService {
+
+	@Override
+	public void execute() throws SystemErrorException, IllegalInputException {
+		List<Employee> empList = new ArrayList<>();
+		EmployeeDAO employeeDAO = new EmployeeDAO();
+		EmployeeNameReader empNameReader = new EmployeeNameReader();
+		String empName = (String) empNameReader.input();
+
+		empList = employeeDAO.findByEmployeeName(empName);
+		ConsoleWriter.showEmployees(empList);
+	}
+
+}
