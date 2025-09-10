6b685ec1392f9a75f8ed27ce2911254401385f8a
j-doniwa <juri-doniwa@kiseki.systems>
2025-09-10 11:21:26 +0900

    add: EmployeeAllFindService.java 作成
    狙い：拡張性の向上
    全件検索のサービスクラスを作成

---
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeAllFindService.java
@@ -0,0 +1,24 @@
+package jp.co.sss.crud.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
+import jp.co.sss.crud.io.ConsoleWriter;
+
+public class EmployeeAllFindService implements IEmployeeService {
+
+	@Override
+	public void execute() throws SystemErrorException, IllegalInputException {
+		List<Employee> empList = new ArrayList<>();
+		EmployeeDAO employeeDao = new EmployeeDAO();
+
+		empList = employeeDao.findAll();
+		ConsoleWriter.showEmployees(empList);
+
+	}
+
+}
